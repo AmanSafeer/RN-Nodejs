e25d5362939cbfc649d35126c4cbbc7d4b1d189d
Aman <codeavenue.mobapp5@gmail.com>
2021-06-02 22:09:20 +0500

    Added CRUD operations on both ends. Modified app UI.

--- App.js
+++ App.js
@@ -6,40 +6,129 @@
  * @flow strict-local
  */
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   SafeAreaView,
   View,
   Text,
   TextInput,
-  TouchableOpacity
+  TouchableOpacity,
+  FlatList
 } from 'react-native';
 import Axios from "axios";
 
 const App = () => {
+  const [_id, setId] = useState("");
   const [name, setName] = useState("");
-  const [message, setMessage] = useState("")
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([{ name: "abc", message: "hello" }]);
+  const [edit, setEdit] = useState(false);
 
-  const submit = () => {
+  useEffect(() => {
+    getMessages()
+  }, [])
+
+  const sendMessage = () => {
     if (name && message) {
-      Axios.post('http://192.168.18.232:3000/', { name, message }, {
-        headers: { 'Content-Type': 'application/json' }
-      })
-        .then(res => res.json())
-        .then(res => console.log("response", res))
+      Axios.post(
+        'http://localhost:3000/send_messages',
+        { name, message },
+        { headers: { 'Content-Type': 'application/json' } }
+      )
+        .then(res => {
+          console.log("send response", res)
+          setName('')
+          setMessage('')
+          getMessages()
+        })
         .catch(err => console.log("error", err))
     }
     else alert("Please enter details")
   }
 
+  const getMessages = () => {
+    Axios.get('http://localhost:3000/get_messages')
+      .then(res => {
+        console.log("get response", res)
+        setMessages(res.data)
+      })
+      .catch(err => console.log(err))
+  }
+
+  const editMessage = (item) => {
+    setId(item._id)
+    setName(item.name)
+    setMessage(item.message)
+    setEdit(true)
+  }
+
+  const updateMessage = () => {
+    const payload = { _id, name, message }
+    Axios.put(
+      "http://localhost:3000/update_messages",
+      payload,
+      { headers: { 'Content-Type': 'application/json' } }
+    )
+      .then(res => {
+        console.log("update response", res)
+        setName('')
+        setMessage('')
+        setEdit(false)
+        getMessages()
+      })
+      .catch(err => console.log("error", err))
+  }
+
+  const deleteMessage = (_id) => {
+    Axios.delete(
+      "http://localhost:3000/delete_messages",
+      { headers: { 'Content-Type': 'application/json' }, data: { _id } }
+    )
+      .then(res => {
+        console.log("delete response", res)
+        getMessages()
+      })
+      .catch(err => console.log("error", err))
+  }
+
   return (
-    <SafeAreaView style={{ flex: 1 }} >
+    <SafeAreaView style={{ flex: 1, }} >
       <View style={{ flex: 1, padding: 20 }}>
         <TextInput placeholder="Name" value={name} onChangeText={setName} style={{ borderWidth: 1, padding: 10 }} />
         <TextInput placeholder="Message" value={message} onChangeText={setMessage} style={{ borderWidth: 1, padding: 10, marginTop: 5 }} />
-        <TouchableOpacity onPress={submit} style={{ marginTop: 20, padding: 10, alignSelf: "center", backgroundColor: "lightgray" }}>
-          <Text>Submit</Text>
-        </TouchableOpacity>
+        {!edit ?
+          <TouchableOpacity onPress={sendMessage} style={{ marginVertical: 15, padding: 10, alignSelf: "center", backgroundColor: "lightgray" }}>
+            <Text>Send</Text>
+          </TouchableOpacity>
+          :
+          <TouchableOpacity onPress={updateMessage} style={{ marginVertical: 15, padding: 10, alignSelf: "center", backgroundColor: "lightgray" }}>
+            <Text>Update</Text>
+          </TouchableOpacity>
+        }
+        <FlatList
+          data={messages}
+          keyExtractor={(item, index) => index.toString()}
+          style={{ backgroundColor: "lightgray" }}
+          renderItem={({ item, index }) => {
+            return (
+              <View style={{ flex: 1, flexDirection: "row", paddingVertical: 20, paddingHorizontal: 5, borderBottomWidth: 1 }}>
+                <View style={{ flex: 1 }}>
+                  <Text style={{ paddingVertical: 2 }}>Name: {item.name}</Text>
+                  <Text style={{ paddingVertical: 2 }}>Message: {item.message}</Text>
+                </View>
+                <View style={{ flexDirection: "row", paddingLeft: 3 }}>
+                  <TouchableOpacity onPress={() => deleteMessage(item._id)} style={{ padding: 10, alignSelf: "center", backgroundColor: "white" }}>
+                    <Text>Del</Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity onPress={() => editMessage(item)} style={{ padding: 10, alignSelf: "center", backgroundColor: "white" }}>
+                    <Text>Edit</Text>
+                  </TouchableOpacity>
+                </View>
+              </View>
+            )
+          }}
+        />
+
       </View>
     </SafeAreaView>
   );
--- Server/index.js
+++ Server/index.js
@@ -1,38 +1,84 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser')
-const MongoClient = require('mongodb').MongoClient
+const MongoDB = require('mongodb')
 const connectionString = "mongodb+srv://testingUser:Testing12345@cluster0.rmqbj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 
-let dbcollection;
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
 
-MongoClient.connect(connectionString, {
+let messagesDb, messagesCollection;
+
+MongoDB.MongoClient.connect(connectionString, {
     useUnifiedTopology: true
 })
     .then((client) => {
+        messagesDb = client.db("Users")
+        messagesCollection = messagesDb.collection("Messages")
         console.log('Connected to Database')
-        const db = client.db("Users")
-        dbcollection = db.collection("Messages")
     })
     .catch((err) => {
-       return console.error(err)
+        return console.error(err)
     })
 
-app.use(bodyParser.urlencoded({ extended: true }))
+app.post('/send_messages', (req, res) => {
+    console.log("post request", req.body)
+    messagesCollection.insertOne(req.body)
+        .then(result => {
+            console.log(result)
+            res.send({ message: "Message has been added" })
+        })
+        .catch(error => {
+            console.log(error)
+            return error
+        })
+})
 
+app.get('/get_messages', (req, res) => {
+    messagesCollection.find().toArray()
+        .then(result => {
+            console.log("Messages", result)
+            res.send(result)
+        })
+        .catch(error => {
+            console.log(error)
+            return error
+        })
+})
 
-app.post('/', (req, res) => {
-    console.log("request", req.body)
-    // dbcollection.insertOne(req.body)
-    // .then(result => {
-    //     // res.redirect('/')
-    //     console.log(result)
-    //     return result
-    // })
-    // .catch(error => {
-    //     console.log(error)
-    //     return error
-    // })
+app.put("/update_messages", (req, res) => {
+    console.log("put request", req.body)
+    messagesCollection.updateOne(
+        { _id: MongoDB.ObjectId(req.body._id) },
+        {
+            $set: {
+                name: req.body.name,
+                message: req.body.message
+            }
+        }
+    )
+        .then(result => {
+            console.log(console.log("Messages", result))
+            res.send({ message: "Messages has been updated" })
+        })
+        .catch(error => {
+            console.log("error", error)
+            return error
+        })
+})
+
+app.delete("/delete_messages", (req, res) => {
+    console.log("delete request", req.body)
+    messagesCollection.deleteOne(
+        { _id: MongoDB.ObjectId(req.body._id) }
+    )
+        .then(result => {
+            console.log(console.log("Messages", result))
+            res.send({ message: "Messages has been deleted" })
+        })
+        .catch(error => {
+            console.log("error", error)
+            return error
+        })
 })
 
 // app.get('/', (req, res) => {
