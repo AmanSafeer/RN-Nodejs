579a551cf99d776b7d7bfdc6cb7588ccf1277e23
Aman <codeavenue.mobapp5@gmail.com>
2021-06-09 21:59:22 +0500

    Added routes and common functions.

--- Server/config/lib/index.js
+++ Server/config/lib/index.js
@@ -1,4 +1,5 @@
 const nodemailer = require("nodemailer");
+const multer = require('multer')
 
 const Transport = async () => {
     try {
@@ -24,6 +25,16 @@ const Transport = async () => {
 
 }
 
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './public/images')
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    }
+})
+
 module.exports = {
-    Transport
+    Transport,
+    storage
 }
